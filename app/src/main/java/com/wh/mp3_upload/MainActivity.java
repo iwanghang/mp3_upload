554a31b065c3144d2e9fc976eebcd1fc21243766
“iwanghang” <iwanghang@qq.com>
2023-05-09 13:31:30 +0800

    Initial commit

---
+++ app/src/main/java/com/wh/mp3_upload/MainActivity.java
@@ -0,0 +1,14 @@
+package com.wh.mp3_upload;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
